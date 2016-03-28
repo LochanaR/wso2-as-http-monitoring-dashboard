cee777dbb6b62d87a778f4a71af14cd5b0809ec2
LochanaR <lochanar@wso2.com>
2016-03-28 16:56:51 +0530

    using DAS Analytics REST API in os, device and browser pie-chart gadgets

--- webapp-analytics/api/modules/technology-stat.js
+++ webapp-analytics/api/modules/technology-stat.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -68,73 +68,22 @@ function getDeviceAllRequests(){
     }
 }
 
+function getOperatingSystemAllRequests(conditions){
+    var results = getAggregateDataFromDAS(OPERATING_SYSTEM_TABLE, conditions, "0", ALL_FACET, [
+        {
+            "fieldName": AVERAGE_REQUEST_COUNT,
+            "aggregate": "SUM",
+            "alias": "SUM_" + AVERAGE_REQUEST_COUNT
+        }
+    ]);
 
-var dbMapping = {
-    'browser': {
-        'table': 'USER_AGENT_FAMILY',
-        'field': 'userAgentFamily'
-    },
-    'os': {
-        'table': 'OPERATING_SYSTEM',
-        'field': 'operatingSystem'
-    },
-    'device-type': {
-        'table': 'DEVICE_TYPE',
-        'field': 'deviceCategory'
+    results = JSON.parse(results);
+
+    if (results.length > 0) {
+        return results[0]['values']['SUM_' + AVERAGE_REQUEST_COUNT];
     }
-};
-
-function buildTechnologySql(dbEntry, whereClause) {
-    return 'SELECT ' + dbEntry.field + ' as name, ' +
-           'sum(averageRequestCount) as request_count, ' +
-           'round((sum(averageRequestCount)*100/(select sum(averageRequestCount) ' +
-           'FROM ' + dbEntry.table + ' ' + whereClause + ')), 2) as percentage_request_count ' +
-           'FROM ' + dbEntry.table + ' ' + whereClause +
-           ' GROUP BY ' + dbEntry.field +
-           ' ORDER BY percentage_request_count DESC;';
 }
 
-function getTechnologyStatData(conditions, type) {
-    var dbEntry = dbMapping[type];
-    var sql = buildTechnologySql(dbEntry, conditions.sql);
-    return executeQuery(sql, conditions.params);
-}
-
-function getTechnologyStat(conditions, type, visibleNumbers, groupName) {
-    var dataObject = {};
-    var i, len;
-    var row;
-    var series;
-    var data;
-    var chartOptions = {};
-
-    var results = getTechnologyStatData(conditions, type);
-
-    var shrinkedResults = helper.getShrinkedResultset(results, visibleNumbers, groupName);
-
-    for (i = 0, len = shrinkedResults.length; i < len; i++) {
-        row = shrinkedResults[i];
-        series = 'series' + i;
-        data = {'label': row['name'], 'data': row['request_count']};
-        dataObject[series] = data;
-    }
-
-    print([dataObject, chartOptions]);
-}
-
-function getTechnologyTubularStat(conditions, type, tableHeadings, sortColumn) {
-    print(helper.getTabularData(getTechnologyStatData(conditions, type), tableHeadings, sortColumn));
-}
-
-//function buildHttpStatusSql(whereClause) {
-//    return 'SELECT responseHttpStatusCode as name, ' +
-//           'sum(averageRequestCount) as request_count, ' +
-//           'round((sum(averageRequestCount)*100/(select sum(averageRequestCount) ' +
-//           'FROM HTTP_STATUS ' + whereClause + ')),2) as percentage_request_count ' +
-//           'FROM HTTP_STATUS ' + whereClause +
-//           ' GROUP BY responseHttpStatusCode ' +
-//           'ORDER BY percentage_request_count DESC;';
-//}
 
 function getHttpStatusStatData(conditions) {
     var output = [];
@@ -232,6 +181,129 @@ function getDeviceStatData(conditions){
     return output;
 }
 
+function getOperatingStatData(conditions){
+    var output = [];
+    var i, total_request_count;
+    var results, result;
+
+    total_request_count = getOperatingSystemAllRequests(conditions);
+
+    if(total_request_count <= 0){
+        return;
+
+    }
+    results = getAggregateDataFromDAS(OPERATING_SYSTEM_TABLE, conditions, "0", OPERATING_SYSTEM_FACET, [
+        {
+            "fieldName": AVERAGE_REQUEST_COUNT,
+            "aggregate": "SUM",
+            "alias": "SUM_" + AVERAGE_REQUEST_COUNT
+        }
+    ]);
+
+    results = JSON.parse(results);
+
+    if (results.length > 0) {
+        for (i = 0; i < results.length; i++) {
+            result = results[i]['values'];
+            output.push([result[OPERATING_SYSTEM_FACET], result['SUM_' + AVERAGE_REQUEST_COUNT],
+                (result['SUM_' + AVERAGE_REQUEST_COUNT]*100/total_request_count).toFixed(2)]);
+        }
+    }
+
+    return output;
+}
+
+
+
+function getHttpStatusTabularStat(conditions, tableHeadings, sortColumn) {
+    print(helper.getTabularData(getHttpStatusStatData(conditions), tableHeadings, sortColumn));
+}
+
+function getBrowserTabularStat(conditions, tableHeadings, sortColumn){
+    print(helper.getTabularData(getBrowserStatData(conditions), tableHeadings, sortColumn));}
+
+function getDeviceTabularStat(conditions, tableHeadings, sortColumn){
+    print(helper.getTabularData(getDeviceStatData(conditions), tableHeadings,sortColumn));
+}
+
+function getOperatingSystemTabularStat(conditions, tableHeadings, sortColumn){
+    print(helper.getTabularData(getOperatingStatData(conditions), tableHeadings, sortColumn));
+}
+
+function getBrowserPieStat(conditions, visibleNumbers, groupName){
+    var dataObject = {};
+    var i, len;
+    var row;
+    var series;
+    var data;
+    var chartOptions = {};
+
+    var results = getBrowserStatData(conditions);
+
+    var shrinkedResults = helper.getShrinkedResultset(results, visibleNumbers, groupName);
+
+    for (i = 0, len = shrinkedResults.length; i < len; i++) {
+        row = shrinkedResults[i];
+        series = 'series' + i;
+        data = {'label': row[0][0], 'data': row[2]};
+        dataObject[series] = data;
+    }
+
+    print([dataObject, chartOptions]);
+
+}
+
+function getDevicePieStat(conditions, visibleNumbers, groupName){
+    var dataObject = {};
+    var i, len;
+    var row;
+    var series;
+    var data;
+    var chartOptions = {};
+
+    var results = getDeviceStatData(conditions);
+
+    var shrinkedResults = helper.getShrinkedResultset(results, visibleNumbers, groupName);
+
+    for (i = 0, len = shrinkedResults.length; i < len; i++) {
+        row = shrinkedResults[i];
+        series = 'series' + i;
+        data = {'label': row[0][0], 'data': row[2]};
+        dataObject[series] = data;
+    }
+
+    print([dataObject, chartOptions]);
+}
+
+function getOperatingSystemPieStat(conditions, visibleNumbers, groupName){
+    var dataObject = {};
+    var i, len;
+    var row;
+    var series;
+    var data;
+    var chartOptions = {};
+
+    var results = getOperatingStatData(conditions);
+
+    var shrinkedResults = helper.getShrinkedResultset(results, visibleNumbers, groupName);
+
+    for (i = 0, len = shrinkedResults.length; i < len; i++) {
+        row = shrinkedResults[i];
+        series = 'series' + i;
+        data = {'label': row[0][0], 'data': row[2]};
+        dataObject[series] = data;
+    }
+
+    print([dataObject, chartOptions]);
+
+}
+
+
+
+
+
+//old
+
 function getHttpStatusStat(conditions) {
     var dataArray = [];
     var ticks = [];
@@ -261,16 +333,4 @@ function getHttpStatusStat(conditions) {
         {'series1': {'label': 's', 'data': dataArray}}, chartOptions
     ]);
 
-}
-
-function getHttpStatusTabularStat(conditions, tableHeadings, sortColumn) {
-    print(helper.getTabularData(getHttpStatusStatData(conditions), tableHeadings, sortColumn));
-}
-
-function getBrowserTabularStat(conditions, tableHeadings, sortColumn){
-    print(helper.getTabularData(getBrowserStatData(conditions), tableHeadings, sortColumn));
-}
-
-function getDeviceTabularStat(conditions, tableHeadings, sortColumn){
-    print(helper.getTabularData(getDeviceStatData(conditions), tableHeadings,sortColumn));
 }
